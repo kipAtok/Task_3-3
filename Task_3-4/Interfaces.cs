549197ad07b88c786ba81c0f4d58f63c8cb2d6bd
NenVan155 <i.nenov155@gmail.com>
2024-02-05 23:49:02 +0300

    1st commit

---
+++ Task_3-4/Interfaces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+interface IPrinter
+{
+    public void Print(string message);
+}
+
+
+interface IArrBase
+{
+    public int Avg { get; }
+
+    public void FillArr(bool userFill = false) 
+    { 
+        if (userFill)
+        {
+            ManualFill();
+        }
+        else
+        {
+            AutoFill();
+        }
+    }
+
+    protected void ManualFill() { }
+
+    protected void AutoFill() { } 
+}
