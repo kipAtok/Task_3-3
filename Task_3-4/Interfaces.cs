1d7afcbd0b04686a22047dc76a17042bd83c37c6
kipAtok <157510037+kipAtok@users.noreply.github.com>
2024-02-07 07:29:54 +0000

    done?

--- Task_3-4/Interfaces.cs
+++ Task_3-4/Interfaces.cs
@@ -5,7 +5,7 @@ interface IPrinter
     public void Print();
 }
 
-interface IArrBase
+interface IArrBase: IPrinter
 {
     public double Avg { get; }
     public void FillArr(bool userFill = false);
--- Task_3-4/Program.cs
+++ Task_3-4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     public static void Main(string[] args)
     {
+        IArrBase[] arrs = new IArrBase[3];
+        arrs[0] = new OneDimentionArr();
+        arrs[1] = new TwoDimentionArr();
+        arrs[2] = new LadderArr();
 
+        foreach (var arr in arrs)
+        {
+            arr.Print();
+            Console.WriteLine($"Avg: {arr.Avg}");
+            arr.FillArr(true);
+            arr.Print();
+        }
+
+        Printer printer = new Printer();
+        IPrinter[] printable = new IPrinter[4];
+        for (int i = 0; i < 3; i++)
+        {
+            printable[i] = arrs[i];
+        }
+        printable[3] = printer;
+
+        foreach (var elem in printable)
+        {
+            elem.Print();
+        }
     }
 }
